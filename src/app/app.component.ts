0c0bf1c6ff9f35d7370b65ff9bf85f52b495ad7b
Raul <raulplaza208@gmail.com>
2025-05-05 19:46:12 +0200

    Mejorado la primera pagina hay que seguir con el login y registro de usuarios para poder acceder al panel de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import {ProductosComponent} from "./components/productos/productos.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,  // Marca el componente como standalone
-  imports: [HeaderComponent, FooterComponent, ProductosComponent]  // Importamos los componentes standalone
+  imports: [HeaderComponent, FooterComponent, RouterModule]  // Importamos los componentes standalone
 })
 export class AppComponent {
   title = 'PlazaShop';  // Propiedad de ejemplo
