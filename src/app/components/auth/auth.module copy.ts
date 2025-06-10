d616d112be18dbdc2fe4f78ccf22e32d949611c3
Raul <raulplaza208@gmail.com>
2025-06-10 09:58:42 +0200

    Terminado la parte del Front

--- src/app/components/auth/auth.module copy.ts
+++ src/app/components/auth/auth.module copy.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';  
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login.component';
-import { RegisterComponent } from './register.component';
+import { LoginComponent } from '../auth/login/login.component';
+import { RegisterComponent } from '../auth/register/register.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
